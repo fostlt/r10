b245254bfb08895ab56d1206be0e0110583caa1e
fostlt <lanceadamp@gmail.com>
2020-03-02 22:33:44 -0800

    Structure for Session added

---
+++ js/screens/Session/Session.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles';
+
+const Session = ({}) => {
+    return(
+        console.log("stuff")
+    )
+}
+
+export default Session;
---
+++ js/screens/Session/SessionContainer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import SessionContainer from './SessionContainer';
+
+class SessionContainer extends Component {
+  render() {
+    return console.log('Stuff');
+  }
+}
+
+export default SessionContainer;
