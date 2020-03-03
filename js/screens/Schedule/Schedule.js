67d5ca359aaf9e7646dc5056cebc9a57d36c0f66
fostlt <lanceadamp@gmail.com>
2020-03-02 22:05:14 -0800

    Schedule structure added

---
+++ js/screens/Schedule/Schedule.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Schedule = ({}) => {
+    return(
+        console.log("stuff")
+    )
+}
+
+
+export default Schedule;
---
+++ js/screens/Schedule/ScheduleContainer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import Schedule from './Schedule';
+
+class ScheduleContainer extends Component {
+  render() {
+    return console.log('Stuff');
+  }
+}
+
+export default ScheduleContainer;
