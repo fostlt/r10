aee79eb9c879ef481a44974f1ac9d2a706835a7a
fostlt <lanceadamp@gmail.com>
2020-03-03 09:36:00 -0800

    Added Map Screen

---
+++ js/screens/Map/Map.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles';
+
+const Map = ({}) => {
+    return(
+        console.log("stuff")
+    )
+}
+
+export default Map;
---
+++ js/screens/Map/MapContainer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import MapContainer from './MapContainer';
+
+class MapContainer extends Component {
+  render() {
+    return console.log('Stuff');
+  }
+}
+
+export default MapContainer;
